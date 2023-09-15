4b15e3872a3639f6bb2f18e581da93952ad15507
Florent Teissier <teissierflorent@gmail.com>
2023-09-15 09:52:42 +0200

    replace jest by vitest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  testPathIgnorePatterns: ["<rootDir>/lib"],
-  transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
-  },
-};
--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,6 +1,7 @@
+import { describe, expect, test } from "vitest";
 import { add } from "../index";
 
 describe("Basic test", () =>
-  it("should be true", () => {
+  test("should be true", () => {
     expect(add(1, 2)).toBe(3);
   }));
